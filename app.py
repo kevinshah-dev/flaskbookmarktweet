0f7f537dc8fb095126d20698eb8e57c1f60802dd
Kevin Shah <ks539@duke.edu>
2024-08-02 18:07:34 -0400

    Removed comma

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def twitter_callback():
     try:
         token = auth.fetch_token(session["authorization_url"])
         print(f"Token: {token}")
-        client = tweepy.Client(bearer_token=token['access_token'], )
+        client = tweepy.Client(bearer_token=token['access_token'])
         print("Is this hitting?")
         print(client_id)
         print(client_secret)
